64da489b4c62ff2f8414a43acde673d670ab20d2
Igor Tsvetkov <igor.ts@plavno.io>
2024-11-15 15:30:10 +0300

    add default room with all affiliations users,default roaster,admin; update README.md update mod_muc in ejabberd.yml.example (copy-pasted from legacy ejabberd config)

--- README.md
+++ README.md
@@ -29,13 +29,34 @@ Ensure the following are installed on your machine:
     ```
     This command will set up and start both the ejabberd and PostgreSQL services as defined in the docker-compose.yml file.
 
-5. Register admin user:
-    ```
-    docker exec -it ejabberd ejabberdctl register admin localhost passw0rd
-    ```
-    Replace `passw0rd` with your desired password.
+
 ## Usage
 After running docker compose up, the following URLs and ports are available:
 
 - Web Admin Interface: http://localhost:5280/admin
 - To connect an XMPP client, use localhost as the server address, with the default XMPP client port `5222`.
+
+## First start 
+On the first start `docker compose up` executes `setup-ejabberd.sh` to pre-configure some default users, rooms, roasters:
+### Users
+- `user1@localhost` password `user1password`
+- `user2@localhost` password `user2password`
+- `user3@localhost` password `user3password`
+- `user4@localhost` password `user4password`
+- `user5@localhost` password `user5password`
+
+All users listed above have been added to the pre-configured room __chatroom1@localhost__
+with next affilations:
+- `user1@localhost` affilation `member`
+- `user2@localhost` affilation `member`
+- `user3@localhost` affilation `admin`
+- `user4@localhost` affilation `owner`
+- `user5@localhost` affilation `outcast` (=banned)
+
+Also `user1` and `user2` by default added to each other __roasters__
+### Global admin
+`admin@localhost` password `adminpassword`
+#### Note
+if you want to rerun `setup-ejabberd.sh`(reset users) the only option for now is to remove volumes:
+  - `docker compose down -v`
+  - `docker compose up` 
---
+++ setup-ejabberd.sh
@@ -0,0 +1,85 @@
+# Check if the initialization has been done
+chmod -R 777 /opt
+
+if [ ! -f /opt/initialized ]; then
+  echo "Initializing ejabberd..."
+
+  # Check if ejabberd is already running (if not, we start it)
+  if ! ejabberdctl status > /dev/null 2>&1; then
+    echo "ejabberd is not running, starting ejabberd..."
+    ejabberdctl start
+  fi
+
+  # Wait for ejabberd to be fully started
+  echo "Waiting for ejabberd to start..."
+  until ejabberdctl status > /dev/null 2>&1; do
+    sleep 1
+  done
+
+
+  if ! ejabberdctl registered_users | grep -q "admin@localhost"; then
+    ejabberdctl register admin localhost adminpassword
+  fi
+  
+  # Add users (if they don't already exist)
+  if ! ejabberdctl registered_users | grep -q "user1@localhost"; then
+    ejabberdctl register user1 localhost user1password
+  fi
+
+  if ! ejabberdctl registered_users | grep -q "user2@localhost"; then
+    ejabberdctl register user2 localhost user2password
+  fi
+
+  if ! ejabberdctl registered_users | grep -q "user3@localhost"; then
+    ejabberdctl register user3 localhost user3password
+  fi
+
+  if ! ejabberdctl registered_users | grep -q "user4@localhost"; then
+    ejabberdctl register user4 localhost user4password
+  fi
+
+  if ! ejabberdctl registered_users | grep -q "user5@localhost"; then
+    ejabberdctl register user5 localhost user5password
+  fi
+
+  echo "Add user1,user2 to each other rosters"
+  ejabberdctl add_rosteritem user1 localhost user2 localhost User2 friends,colleagues both
+
+  # Create a chat room (if it doesn't already exist)
+  if ! ejabberdctl get_room_occupants_number chatroom1 conference.localhost > /dev/null 2>&1; then
+    echo "Create 'chatroom1'"
+    ejabberdctl create_room_with_opts chatroom1 conference.localhost localhost persistent:true
+    
+    # Add user1 to chatroom1 as member
+    echo "Add user1 to chatroom1 as member (affiliation)"
+    ejabberdctl set_room_affiliation chatroom1 conference.localhost user1@localhost member
+    
+    # Add user2 to chatroom1 as member
+    echo "Add user2 to chatroom1 as member (affiliation)"
+    ejabberdctl set_room_affiliation chatroom1 conference.localhost user2@localhost member
+    
+    # Add user3 to chatroom1 as admin
+    echo "Add user3 to chatroom1 as admin (affiliation)"
+    ejabberdctl set_room_affiliation chatroom1 conference.localhost user3@localhost admin
+    
+    # Add user4 to chatroom1 as owner
+    echo "Add user4 to chatroom1 as owner (affiliation)"
+    ejabberdctl set_room_affiliation chatroom1 conference.localhost user4@localhost owner
+    
+    # Add user5 to chatroom1 as outcast
+    echo "Add user5 to chatroom1 as outcast (affiliation)"
+    ejabberdctl set_room_affiliation chatroom1 conference.localhost user5@localhost outcast
+  fi
+
+  # Mark initialization as done
+  touch /opt/initialized
+  echo "Initialization complete."
+  ejabberdctl stop
+  echo "Waiting for ejabberd to stop...(will be started as docker main process soon)"
+  until ! ejabberdctl status > /dev/null 2>&1; do
+    sleep 1
+  done
+  echo "ejabberd has stopped"
+else
+  echo "ejabberd already initialized."
+fi
